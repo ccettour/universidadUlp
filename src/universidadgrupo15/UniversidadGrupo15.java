ee5dcf022c89bb1fcb1cb456f4c30f1dda4cdf22
Christian <christiancettour@gmail.com>
2023-09-07 18:02:07 -0300

    Paquetes y entidades
    Agrego los 3 paquetes y las 3 entidades

---
+++ src/universidadgrupo15/UniversidadGrupo15.java
@@ -0,0 +1,9 @@
+package universidadgrupo15;
+
+public class UniversidadGrupo15 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/universidadgrupo15/entidades/Inscripcion.java
@@ -0,0 +1,66 @@
+package universidadgrupo15.entidades;
+
+public class Inscripcion {
+    private int idInscripcion;
+    private int nota;
+    private Alumno alumno;
+    private Materia materia;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int nota, Alumno alumno, Materia materia) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion(int idInscripcion, int nota, Alumno alumno, Materia materia) {
+        this.idInscripcion = idInscripcion;
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    @Override
+    public String toString() {
+        String inscrip = idInscripcion + " " + alumno.getApellido() + " " + alumno.getNombre() + " " + materia.getNombre();
+        return inscrip;
+    }
+
+   
+    
+    
+}
