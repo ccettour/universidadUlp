7bb52d0966aa09a1d562738b1cf476a5561d98e7
Christian <christiancettour@gmail.com>
2023-09-08 17:55:46 -0300

    update conexion y alumnos
    Agrego clases conexion,  alumnoData y unas vistas de prueba

--- src/universidadgrupo15/UniversidadGrupo15.java
+++ src/universidadgrupo15/UniversidadGrupo15.java
@@ -1,9 +1,25 @@
 package universidadgrupo15;
 
+import accesoADatos.AlumnoData;
+import java.time.LocalDate;
+import universidadgrupo15.entidades.Alumno;
+
 public class UniversidadGrupo15 {
 
     public static void main(String[] args) {
+        Alumno alumno = new Alumno(15986523,"Lopez","María",LocalDate.of(1980,4,25),true);
+        AlumnoData alu=new AlumnoData();
+        alu.guardarAlumno(alumno);
         
+        //alu.modificarAlumno(alumno);
+        
+        // alu.eliminarAlumno(1);
+        
+        Alumno aluEncontrado = alu.buscarAlumno(2);
+        if(aluEncontrado!=null){
+            
+            System.out.println(aluEncontrado.toString());
+        }
     }
     
 }
