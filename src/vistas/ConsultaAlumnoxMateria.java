4b88e86871ad1ba9f89ae15ddbb0f13f7838134b
Juan Castro <juanboffi44@gmail.com>
2023-09-19 18:37:06 -0300

    ygy

--- src/vistas/ConsultaAlumnoxMateria.java
+++ src/vistas/ConsultaAlumnoxMateria.java
@@ -86,16 +86,9 @@ InscripcionData ind=new InscripcionData();
                 "ID", "DNI", "Apellido", "Nombre"
             }
         ) {
-            Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
-            };
             boolean[] canEdit = new boolean [] {
                 false, false, false, false
             };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
